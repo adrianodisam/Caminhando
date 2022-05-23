c65fc923bc8155f3ce3961f01af7c09f3eb8ee56
Adrianodisam <adrianodisam@gmail.com>
2022-05-23 16:38:28 -0300

    envio do projeto ong

--- src/components/Sobre/CardsSobre.js
+++ src/components/Sobre/CardsSobre.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from './CardsSobre.module.css';
-import ajudaImg from '../imgs/002-heart 3.png';
-const CardsSobre = () => {
+
+const CardsSobre = (props) => {
   return (
-    <div className={styles.sobreCard}>
-      <img src={ajudaImg} alt="ajuda" />
-    </div>
+    <>
+      <div className={styles.sobreCard}>
+        <div className={props.cores}>
+          <div className={styles.divImg}>
+            <div className={styles.circuloImg}>
+              <img src={props.imagem} alt={props.texto} />
+            </div>
+          </div>
+
+          <footer className={styles.textoPadrao}>
+            <p className={styles.textoP}>{props.texto}</p>
+          </footer>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/components/Sobre/Sobre.js
+++ src/components/Sobre/Sobre.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Sobre.module.css';
 import ajudaImg from '../imgs/002-heart 3.png';
+import CardsSobre from './CardsSobre';
 const Sobre = () => {
   return (
     <div className={styles.sobre}>
@@ -9,19 +10,21 @@ const Sobre = () => {
         <p className={styles.instSobre}>Conheça a instituição </p>
       </div>
       <div className={styles.divSobreCard}>
-        <div className={styles.sobreCard}>
-          <img src={ajudaImg} alt="" />
-
-          <p>Ajude a instituição</p>
-        </div>
-        <div className={styles.sobreCard}>
-          <img src={ajudaImg} alt="" />
-          <p>Nossos projetos</p>
-        </div>
-        <div className={styles.sobreCard}>
-          <img src={ajudaImg} alt="" />
-          <p>quem somos</p>
-        </div>
+        <CardsSobre
+          imagem={ajudaImg}
+          texto="Nossos projetos"
+          cores={styles.divSobre}
+        />
+        <CardsSobre
+          imagem={ajudaImg}
+          texto="Nossos projetos"
+          cores={styles.divSobre}
+        />
+        <CardsSobre
+          imagem={ajudaImg}
+          texto="Nossos projetos"
+          cores={styles.divSobre}
+        />
       </div>
     </div>
   );
